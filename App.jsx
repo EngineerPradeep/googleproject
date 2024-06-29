8178b13e840b03ab60a495f003d946bc655c721e
EngineerPradeep <104459028+EngineerPradeep@users.noreply.github.com>
2024-06-29 21:35:22 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import KgButton from "./KgButton";
+
+function App() {
+  return;
+  <div>
+    <h1>hello World the best React Course</h1>
+    <button>Subscribe the button</button>
+
+    <KgButton></KgButton>
+  </div>;
+}
+
+export default App;
---
+++ KgButton.jsx
@@ -0,0 +1,11 @@
+function KgButton() {
+  return;
+  <div>
+    <button>This is my button</button>
+    <button>This is my button</button>
+    <button>This is my button</button>
+    <button>This is my button</button>
+  </div>;
+}
+
+export default KgButton;
